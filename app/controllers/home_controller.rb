d76692b18c0965eb9bacd3ce8cbe5730638f94f0
Manalsu <manalsujuneja@hotmail.com>
2016-06-07 00:25:38 +0530

    init

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
 	
 	def home
-	end
-
+    @strips = Strip.all
+  end
+    
 end
--- app/models/strip.rb
+++ app/models/strip.rb
@@ -1,3 +1,3 @@
 class Strip < ActiveRecord::Base
-	has_many :projects
+	has_many :projects, dependent: :destroy
 end
