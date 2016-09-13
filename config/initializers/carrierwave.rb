a15438bd5a6c53487831d90d07248908525d8b00
Manalsu <manalsujuneja@hotmail.com>
2016-09-13 17:33:14 +0530

    init

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,7 +4,8 @@ CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider              => 'AWS',
     :aws_access_key_id     => ENV['AWS_ACCESS_KEY_ID'],
-    :aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    :aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
+    :region => 'us-west-2'
   }
 
   if Rails.env.test? || Rails.env.cucumber?
