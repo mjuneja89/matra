e107880ed2b5e7ee88ed1c2a505d353e39e8d375
Manalsu <manalsujuneja@hotmail.com>
2016-09-13 17:22:56 +0530

    init

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,8 +4,7 @@ CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider              => 'AWS',
     :aws_access_key_id     => ENV['AWS_ACCESS_KEY_ID'],
-    :aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
-    :region => 'us-west-2'
+    :aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
   }
 
   if Rails.env.test? || Rails.env.cucumber?
@@ -23,14 +22,4 @@ end
 
 end
 
-module CarrierWave
-  module MiniMagick
-    def quality(percentage)
-      manipulate! do |img|
-        img.quality(percentage.to_s)
-        img = yield(img) if block_given?
-        img
-      end
-    end
-  end
 end
