03e134c3774003163ec1a0f272c1acd7a2925d61
Manalsu <manalsujuneja@hotmail.com>
2016-09-20 16:31:15 +0530

    init

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -36,7 +36,7 @@ class UsersController < ApplicationController
     private
 
     def user_params
-       params.require(:user).permit(:first_name, :last_name, :email, :password, :avatar)
+       params.require(:user).permit(:first_name, :last_name, :email, :password, :avatar, :avatar_category)
     end
 
 end
