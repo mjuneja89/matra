ca1d64fafd12f5e90265722134190b465db2f735
mjuneja89 <manalsujuneja@hotmail.com>
2016-12-12 15:34:36 +0530

    init

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -5,12 +5,16 @@ class SearchesController < ApplicationController
       project_ids = []
       award_ids = []
       publication_ids = []
-      project_ids << Project.where('first_name ILIKE ?', "%#{params[:search]}%").all.map(&:id) 
+      project_ids << Project.where('first_name ILIKE ?', "%#{params[:search]}%").all.map(&:id)
+      project_ids << Project.where('last_name ILIKE ?', "%#{params[:search]}%").all.map(&:id) 
       project_ids << Project.where('location ILIKE ?', "%#{params[:search]}%").all.map(&:id)
+      project_ids << Project.where('yearcompleted ILIKE ?', "%#{params[:search]}%").all.map(&:id)
       @projects = Project.where(id: project_ids)
       award_ids << Award.where('first_name ILIKE ?', "%#{params[:search]}%").all.map(&:id) 
+      award_ids << Award.where('last_name ILIKE ?', "%#{params[:search]}%").all.map(&:id) 
       @awards = Award.where(id: award_ids)
       publication_ids << Publication.where('first_name ILIKE ?', "%#{params[:search]}%").all.map(&:id) 
+      publication_ids << Publication.where('last_name ILIKE ?', "%#{params[:search]}%").all.map(&:id) 
       @publications = Publication.where(id: publication_ids)
     end
    end
