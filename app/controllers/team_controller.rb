f0b83097f33c8b3b994fd64c8a2ad716b32265c0
mjuneja89 <manalsujuneja@hotmail.com>
2017-02-02 19:59:19 +0530

    init

--- app/controllers/team_controller.rb
+++ app/controllers/team_controller.rb
@@ -1,7 +1,7 @@
 class TeamController < ApplicationController
   
   def team
-  	@users = User.order(:id).page(params[:page]).per(7)
+  	@users = User.where.not(id: 1).order(:id).page(params[:page]).per(7)
   end
   
 end
